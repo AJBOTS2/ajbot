363dfc5acc6ea91d48d872daae0160dbcde2cf35
AJBOTS2 <gamerff86420@gmail.com>
2025-02-02 19:29:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -109,9 +109,9 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 # Token Verification Info :
 VERIFY = bool(environ.get('VERIFY', False))
-VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', '')
-VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', '')
-VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', '')
+VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'Linkspay.in')
+VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', '4eb4c465c67b29ff5ea7478bf729fd67671b05be')
+VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/tokenprocess_video/4')
 
 # If You Fill Second Shortner Then Bot Attach Both First And Second Shortner And Use It For Verify.
 VERIFY_SECOND_SHORTNER = bool(environ.get('VERIFY_SECOND_SHORTNER', False))
@@ -121,7 +121,7 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 
 
 # Shortlink Info
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False)) # Set True Or False
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'https://Linkspay.in')
 SHORTLINK_API = environ.get('SHORTLINK_API', '4eb4c465c67b29ff5ea7478bf729fd67671b05be')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/tokenprocess_video/4') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
