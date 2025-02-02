5f16fe5a54b42eb26707d6b260d4d8d1391da783
AJBOTS2 <gamerff86420@gmail.com>
2025-02-02 19:18:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -124,7 +124,7 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'ttps://Linkspay.in')
 SHORTLINK_API = environ.get('SHORTLINK_API', '4eb4c465c67b29ff5ea7478bf729fd67671b05be')
-TUTORIAL = environ.get('TUTORIAL', 'https://t.me/tokenprocess_video/4'') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/tokenprocess_video/4') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
 
 
 # Others
